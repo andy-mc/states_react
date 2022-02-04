865238d00014edfa0433c675918103237daa5bc7
Andrés Muñoz <andres.munoz@idukay.com>
2022-02-03 23:22:15 -0500

    move setStates into functions that declare events

--- src/UseState.js
+++ src/UseState.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useCallback} from "react";
 import { useStateMany } from "./hooks/useStateMany";
 const SECURITY_CODE = "paradigma";
 
@@ -12,18 +12,48 @@ const UseState = ({name}) => {
   });
 
   const {code, confirmed, deleted, loading, error} = state;
+  
+  const onConfirm = useCallback(() => {
+    setState({ confirmed: true, deleted: false, error: false, loading: false })
+  }, [setState])
+
+  const onError = useCallback(() => {
+    setState({ error: true, loading: false })
+  }, [setState])
+
+  const onWrite = (value)=> {
+    setState({code: value});
+  }
+
+  const onCheck = () => {
+    setState({loading: true});
+  }
+
+  const onDelete = () => {
+    setState({confirmed: false, deleted: true})
+  }
+
+  const onReset = () => {
+    setState({
+      code:'',
+      confirmed: false,
+      deleted: false,
+      loading: false,
+      error: false,
+    })
+  }
 
   useEffect(() => {
     if (loading) {
       setTimeout(() => {
         if(code === SECURITY_CODE) {
-          setState({ error: false, loading: false, confirmed: true })
+          onConfirm()
         } else {
-          setState({ error: true, loading: false })
+          onError()
         }
       }, 3000);
     }
-  }, [loading, code, setState])
+  }, [onConfirm, onError, code, loading])
 
   if (!confirmed && !deleted) {
     return (
@@ -39,11 +69,11 @@ const UseState = ({name}) => {
         <input type='text' value={code}
           placeholder='código de seguridad'
           onChange={({target: {value}}) => {
-            setState({code: value});
+            onWrite(value)
           }}
         />
         <button onClick={() => {
-          setState({loading: true});
+          onCheck()
         }}>
           Comprobar
         </button>
@@ -54,10 +84,10 @@ const UseState = ({name}) => {
       <>
         <h2>Eliminar {name}</h2>
         <p>¿Seguro que quieres eliminar {name}?</p>
-        <button onClick={() => {setState({confirmed: false, code: ''});}}>
+        <button onClick={() => {onReset()}}>
           No, volver
         </button>
-        <button onClick={() => {setState({confirmed: false, deleted: true});}}>
+        <button onClick={() => {onDelete()}}>
           Sí, eliminar
         </button>
       </>
@@ -66,7 +96,7 @@ const UseState = ({name}) => {
     return (
       <>
         <h2>{name} fue eliminado con éxito !!</h2>
-        <button onClick={() => {setState({deleted: false, code: ''});}}>
+        <button onClick={() => {onReset()}}>
           Recuperar {name}
         </button>
       </>
