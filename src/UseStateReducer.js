876fa6920aed05dacaf7e46cc460bca8dd3d6c2d
Andrés Muñoz <andres.munoz@idukay.com>
2022-02-07 00:13:42 -0500

    fix functions

--- src/UseStateReducer.js
+++ src/UseStateReducer.js
@@ -30,9 +30,7 @@ const UseStateReducer = ({name}) => {
   }
 
   const onCheck = () => dispatch({type: actionsTypes.check});
-
   const onDelete = () => dispatch({type: actionsTypes.delete});
-
   const onReset = () => dispatch({type: actionsTypes.reset});
 
   useEffect(() => {
